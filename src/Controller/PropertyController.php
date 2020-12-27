237dbd28aaf6061d247109fa5def334949d29013
LindaOuer <linda.ouerfelli@gmail.com>
2020-12-27 19:57:54 +0100

    migrations + first property

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,6 +17,24 @@ class PropertyController extends AbstractController
      */
     public function index(): Response
     {
+        /* $property = new Property();
+        $property->setAddress('01 boulvard Habib Bourguiba')
+                ->setBedrooms(3)
+                ->setCity('Tunis')
+                ->setDescription('Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic nisi dolor labore possimus omnis dolore praesentium ullam cum soluta magnam nemo, voluptatibus qui repellat quod libero obcaecati ducimus, doloribus at.')
+                ->setFloor(2)
+                ->setHeat(1)
+                ->setPostalCode(2000)
+                ->setPrice(200000)
+                ->setRooms(3)
+                ->setSurface(100)
+                ->setTitle('Property 1');
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($property);
+        $manager->flush(); */
+
+        
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties'
         ]);
