190b6c6bce23ee87cb64d05d08371ddbf41c8734
LindaOuer <linda.ouerfelli@gmail.com>
2020-12-27 19:23:31 +0100

    First Controller

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PropertyController extends AbstractController
+{
+    /**
+     * Undocumented function
+     *
+     * @return Response
+     * @Route("/properties", name="property_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('property/index.html.twig', [
+            'current_menu' => 'properties'
+        ]);
+    }
+}
