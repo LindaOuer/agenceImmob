fe42efe934d07c424fc00fc8f19f5eec0659cc1d
LindaOuer <linda.ouerfelli@gmail.com>
2020-12-31 12:47:57 +0100

    Revert "added Validators (see Property.php)"
    This reverts commit a68612bb9c9b4903eddb6c8f4f569557e15756c7.

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -5,12 +5,9 @@ namespace App\Entity;
 use App\Repository\PropertyRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
-use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=PropertyRepository::class)
- * @UniqueEntity(fields = {"title"},message ="Title already exists")
  */
 class Property
 {
@@ -33,7 +30,6 @@ class Property
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Assert\Length(min=5, minMessage="Minimum 5 char")
      */
     private $title;
 
@@ -44,7 +40,6 @@ class Property
 
     /**
      * @ORM\Column(type="integer")
-     * @Assert\Range(min=10, max=400)
      */
     private $surface;
 
@@ -85,7 +80,6 @@ class Property
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Assert\Regex("/^[0-9]{5}$/")
      */
     private $postal_code;
 
